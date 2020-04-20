2abd64eabb16e19a207c21003efccedffad25055
carlos <carlospalma03@gmail.com>
2020-04-20 08:41:07 -0500

    changing main image by clicking on it

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -61,7 +61,7 @@ describe('PhoneCat Application', function() {
 		query.sendKeys('nexus');
 
 		element.all(by.css('.phonesDynamic li a')).first().click();
-		expect(browser.getCurrentUrl()).toContain('#!/phones/nexus-s');
+		expect(browser.getCurrentUrl()).toContain('#!/phonedetail/nexus-s');
 	});
   });
 
@@ -75,7 +75,23 @@ describe('PhoneCat Application', function() {
     it('should display the `nexus-s` page', function() {
       expect(element(by.binding('$ctrl.phone.name')).getText()).toBe('Nexus S');
     });
-  
+	
+	it('should display the first phone image as the main phone image', function() {
+      var mainImage = element(by.css('img.phone'));
+
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.0.jpg');
+    });
+
+	 it('should swap the main image when clicking on a thumbnail image', function() {
+      var mainImage = element(by.css('img.phone'));
+      var thumbnails = element.all(by.css('.phone-thumbs img'));
+
+      thumbnails.get(2).click();
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.2.jpg');
+
+      thumbnails.get(0).click();
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.0.jpg');
+    });
   });
 
 
